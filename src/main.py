6bacf3cb1f479ff23f5c5cc6ab31d79be3cca40a
Andy Bulka <abulka@gmail.com>
2021-07-26 14:19:21 +1000

    trying to get site packages visible

--- src/main.py
+++ src/main.py
@@ -1,4 +1,5 @@
 import os, glob
+import requests
 
 print("A simple test python app")
 for i in range(200):
@@ -8,4 +9,7 @@ print()
 print(os.listdir("."))
 print(glob.glob("*.py"))
 print(os.getcwd())
+
+print(requests.get('http://www.google.com').status_code)
+
 print("done.")
